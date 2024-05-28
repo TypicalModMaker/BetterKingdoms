f40909a9cdb77c38bbd48a6bea0a9e4badc3c0ec
TypicalModMaker <@>
2024-05-28 18:01:27 +0200

    feat: codefactor

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # BetterKingdoms
 
+[![CodeFactor](https://www.codefactor.io/repository/github/typicalmodmaker/betterkingdoms/badge/master)](https://www.codefactor.io/repository/github/typicalmodmaker/betterkingdoms/overview/master)
+
 A Kingdoms Plugin for 1.20+
 
 Authors:  
--- src/main/java/dev/isnow/betterkingdoms/util/DateUtil.java
+++ src/main/java/dev/isnow/betterkingdoms/util/DateUtil.java
@@ -8,7 +8,7 @@ import java.util.Locale;
 @UtilityClass
 public class DateUtil {
     public static String formatElapsedTime(long elapsedMilliseconds) {
-        // Break the elapsed seconds into milliseconds and seconds. // From Android
+        // Break the elapsed milliseconds into seconds and milliseconds. From Android
         long seconds = 0;
         long milliseconds;
         if (elapsedMilliseconds >= 1000) {
@@ -17,7 +17,7 @@ public class DateUtil {
         }
         milliseconds = elapsedMilliseconds;
         StringBuilder sb = new StringBuilder(8);
-        ;
+
         Formatter f = new Formatter(sb, Locale.getDefault());
 
         return f.format("%d.%03d", seconds, milliseconds).toString();
